d19f2332b4347acb5efc34bfeeab5e8aae9bd981
Greviansonula <greviansonula@gmail.com>
2023-04-09 20:02:08 +0300

    complete docker compose

--- Dockercompose/app.py
+++ Dockercompose/app.py
@@ -4,13 +4,13 @@ import redis
 import time
 
 app = Flask(__name__)
-cache = redis.Redis(hsot='redis', port=6379)
+cache = redis.Redis(host='redis', port=6379)
 
 def get_hit_count():
     retries = 5
     while True:
         try:
-            cache.reset_retry_count()
+            # cache.reset_retry_count()
             return cache.incr('hits')
         except redis.exceptions.ConnectionError as exc:
             if retries == 0:
@@ -21,8 +21,8 @@ def get_hit_count():
 @app.route('/')
 def hello():
     count = get_hit_count()
-    return "Hello Grevians! I have been seen {} times.\n".format(count)
-    
+    return "Hello There again and again! I have been seen {} times.\n".format(count)
 
-if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+
+# if __name__ == "__main__":
+#     app.run(debug=True, host="0.0.0.0", port=5000)
